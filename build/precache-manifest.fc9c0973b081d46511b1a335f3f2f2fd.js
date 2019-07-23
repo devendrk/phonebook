a9a389d54e3a18118cd0ceaad00542e7afa0601c
devendrk <katwaldevendra@hotmail>
2019-07-23 11:59:01 +0300

    bupdated build

---
+++ build/precache-manifest.fc9c0973b081d46511b1a335f3f2f2fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8181e9efdb8f9ca866c3f3b714a342",
+    "url": "/data-for-countries/index.html"
+  },
+  {
+    "revision": "56862ca13a247a1784a7",
+    "url": "/data-for-countries/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "3db22e5127a5423454ea",
+    "url": "/data-for-countries/static/css/main.ecf7ed79.chunk.css"
+  },
+  {
+    "revision": "56862ca13a247a1784a7",
+    "url": "/data-for-countries/static/js/2.f7ffde4a.chunk.js"
+  },
+  {
+    "revision": "3db22e5127a5423454ea",
+    "url": "/data-for-countries/static/js/main.0483d0e2.chunk.js"
+  },
+  {
+    "revision": "cff361559aeca980c9d9",
+    "url": "/data-for-countries/static/js/runtime~main.76cff5e8.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
 
 const app = express()
-
-
-const bodyParser = require('body-parser')
-
 app.use(bodyParser.json())
-
 morgan.token('id', function getId(req) {
   return req.id
 })
-
 app.use(morgan(':id :method :url :response-time :res[header]'))
+app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
   {
@@ -51,13 +50,11 @@ const generateId = () => {
 
 // add person
 app.post('/persons', (req, res) => {
-
   const person = {
     name: req.body.name,
     number: req.body.number,
     id: generateId()
   }
-  console.log(person)
   if (persons.find(person => person.name === req.body.name)) {
     return res.status(404).send({ error: 'name must be unique' })
 
@@ -66,7 +63,6 @@ app.post('/persons', (req, res) => {
   }
 
   persons = persons.concat(person)
-
   res.json(person)
 
 })
@@ -81,7 +77,6 @@ app.get('/persons/:id', (req, res) => {
   res.json(person)
 })
 
-
 //delete person
 app.delete('/persons/:id', (req, res) => {
   const id = Number(req.params.id)
@@ -89,95 +84,6 @@ app.delete('/persons/:id', (req, res) => {
   res.status(204).end()
 })
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`server is running on ${PORT}......`))
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require('express')
-// const cors = require('cors')
-// // const morgan = require('morgan')
-// // app.use(morgan())
-
-
-// const app = express()
-
-// app.use(express.json())
-// app.use(cors())
-// app.use(express.static('build'))
-
-
-
-// app.get('/', (req, res) => res.send('<h1>Hello World!............!</h1>'))
-
-// app.get('/persons', (req, res) => res.send(phonebook))
-
-// app.get('/persons/:id', (req, res) => {
-//   const contact = phonebook.find(c => c.id === parseInt(req.params.id))
-//   if (!contact) {
-//     res.status(404).send('requested phone book is not available ')
-//   }
-//   res.send(contact)
-// })
-
-// // generate random id
-// const generateId = () => {
-//   const randomId = Math.floor(Math.random() * phonebook.length * 50)
-//   return randomId
-// }
-
-
-// // Add new contact
-// app.post('/persons', (req, res) => {
-
-//   const contact = {
-//     name: req.body.name,
-//     number: req.body.number,
-//     id: generateId()
-//   }
-
-//   if (phonebook.find(contact => contact.name === req.body.name)) {
-//     return res.status(404).send({ error: 'name must be unique' })
-//   } else if (!contact.name || !contact.number) {
-//     return res.status(404).send({ error: "name or number missing" })
-//   }
-
-//   // updates database
-//   phonebook.push(contact);
-
-//   // send updated response to the client
-//   res.send(contact)
-// })
-
-// // Delete contact
-// app.delete('/persons/:id', (req, res) => {
-//   const deleteItem = phonebook.find(item => item.id === parseInt(req.params.id))
-//   if (!deleteItem) {
-//     res.status(404).send({ error: "contact with the id you requestd ,do not exist or already deleted" })
-//   }
-//   const index = phonebook.indexOf(deleteItem)
-//   phonebook.splice(index, 1)
-//   res.send(deleteItem)
-// })
-
-// // get info
-// app.get('persons/info', (req, res) => {
-//   const contactLength = phonebook.length
-//   const timeDate = new Date();
-//   res.send('phoneBook has info for ' + contactLength + ' people' + '</br>' + timeDate)
-// })
-
-// const PORT = process.env.PORT || 3001;
-// app.listen(PORT, () => {
-//   console.log(`Our app is running on port...${PORT}`);
-// });
